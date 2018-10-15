b4c276fae611fedaacadc4202e30eebff5f34aa8
HiperEmmanuel <hiperemmanuel@gmail.com>
2018-10-14 23:48:44 -0600

    PlantillaCorp
    Creador de plantillas HTML para pasar catalogos de imagenes a partir de la lista de imagenes

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lista = new System.Windows.Forms.TextBox();
+            this.plantilla = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // lista
+            // 
+            this.lista.Location = new System.Drawing.Point(13, 13);
+            this.lista.Multiline = true;
+            this.lista.Name = "lista";
+            this.lista.Size = new System.Drawing.Size(100, 236);
+            this.lista.TabIndex = 0;
+            this.lista.Text = "Pegue aqui la lista de elementos";
+            // 
+            // plantilla
+            // 
+            this.plantilla.BackColor = System.Drawing.SystemColors.Info;
+            this.plantilla.Location = new System.Drawing.Point(342, 12);
+            this.plantilla.Multiline = true;
+            this.plantilla.Name = "plantilla";
+            this.plantilla.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.plantilla.Size = new System.Drawing.Size(343, 237);
+            this.plantilla.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(120, 122);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(216, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Ejecutar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(699, 261);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.plantilla);
+            this.Controls.Add(this.lista);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox lista;
+        private System.Windows.Forms.TextBox plantilla;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +13,56 @@ namespace PlantillaCorp
 {
     public partial class Form1 : Form
     {
+
+        ArrayList Vector1;
         public Form1()
         {
             InitializeComponent();
+            Vector1 = new ArrayList();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            plantilla.Clear();
+            
+
+            if (!string.IsNullOrEmpty(Convert.ToString(lista.Text)))
+            {
+                string IdOrder = Convert.ToString(lista.Text.Trim());
+
+                //replacing "enter" i.e. "\n" by "," 
+                string temp = IdOrder.Replace("\r\n", ",");
+
+                string[] ArrIdOrders = Regex.Split(temp, ",");
+
+                for (int z = 0; z < ArrIdOrders.Length; z++)
+                {
+                    Vector1.Add(lista.Text.ToString());
+                    string div = "<div class='uk-width-small-1-1 uk-width-medium-1-3 uk-width-large-1-4'>";
+                    string div1 = "\n <div class='catalogo-grid'>";
+                    string cadena = "\n <img src = '/images/jpg/catalogo/rm/uno/" + Convert.ToString(ArrIdOrders[z]) + ".jpg' alt='"+ Convert.ToString(ArrIdOrders[z]) + "' href='/images/jpg/catalogo/rm/uno/"+ Convert.ToString(ArrIdOrders[z]) + ".jpg' data-uk-lightbox='{group:''}' title='"+ Convert.ToString(ArrIdOrders[z]) + "'><a href = '/' >";
+                    plantilla.Text += div + div1 + cadena + Convert.ToString(ArrIdOrders[z]) + "\n </a><hr></div></div>";
+                }
+            }
+
+            //int i;
+            //for (i=0; i < Vector1.Count; i++) {
+            //    string div  = "<div class='uk-width-small-1-1 uk-width-medium-1-3 uk-width-large-1-4'>";
+            //    string div1 = "< div class='catalogo-grid'>";
+            //    string cadena = "<img src = '/images/jpg/catalogo/rm/uno/RM-203.jpeg' alt='18-R73' href='/images/jpg/catalogo/rm/uno/RM-203.jpeg' data-uk-lightbox='{group:''}' title='RM-203'><a href = '/' >";
+            //    plantilla.Text = div + div1 + cadena + Convert.ToString(Vector1[i])  + "\n </a><hr></div> </div>";
+
+            //}
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
         }
     }
 }
